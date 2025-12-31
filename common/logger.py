a301b3d413e4e61858dcf08c0395e924c0aec2bc
Ian <47256454+IanRFerguson@users.noreply.github.com>
2025-12-30 21:26:36 -0500

    common src

---
+++ common/logger.py
@@ -0,0 +1,35 @@
+import logging
+import os
+import sys
+
+from colorlog import ColoredFormatter
+
+logger = logging.getLogger(__name__)
+_handler = logging.StreamHandler(sys.stdout)
+
+# NOTE - We'll use colored logs only in local development for better visibility
+if os.environ.get("LOCAL") == "true":
+    _formatter = ColoredFormatter(
+        "%(log_color)s%(levelname)s%(reset)s %(message)s",
+        datefmt=None,
+        reset=True,
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",  # Example with background color
+        },
+        secondary_log_colors={},
+        style="%",
+    )
+else:
+    _formatter = logging.Formatter("%(levelname)s %(message)s")
+
+_handler.setFormatter(_formatter)
+logger.addHandler(_handler)
+logger.setLevel("INFO")
+
+if os.environ.get("DEBUG") == "true":
+    logger.setLevel("DEBUG")
+    logger.debug("Logging at debug level")
