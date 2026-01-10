b45b4408395965aa9f74cb2ca6300ec16e62d161
Ian <47256454+IanRFerguson@users.noreply.github.com>
2026-01-09 22:24:49 -0500

    serve with flask

--- app/backend/server.py
+++ app/backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.api import bp as api_blueprint
 from routes.webhook import bp as webhook_blueprint
 
@@ -6,8 +6,19 @@ from common.logger import metrics_logger
 
 #####
 
-server = Flask(__name__)
+server = Flask(
+    __name__,
+    static_folder="frontend/dist",
+    static_url_path="/",
+    template_folder="frontend/dist",
+)
 server.logger = metrics_logger
+
+
+@server.route("/")
+def index():
+    return render_template("index.html")
+
 
 server.register_blueprint(api_blueprint)
 server.register_blueprint(webhook_blueprint)
