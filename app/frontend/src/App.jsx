cffd2fb321974cd71e923e0d9ce390623cd42dce
Ian <47256454+IanRFerguson@users.noreply.github.com>
2026-01-11 17:46:23 -0500

    add password protection

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import './App.css'
 import Header from './components/Header.jsx';
 import WeeklyStats from './components/WeeklyStats.jsx';
+import PasswordGate from './components/PasswordGate.jsx';
 
 function App() {
   return (
-    <>
-      <div>
-        <Header />
-      </div>
-      <div>
-        <WeeklyStats />
-      </div>
-    </>
+    <PasswordGate password="shred">
+      <>
+        <div>
+          <Header />
+        </div>
+        <div>
+          <WeeklyStats />
+        </div>
+      </>
+    </PasswordGate>
   )
 }
 
