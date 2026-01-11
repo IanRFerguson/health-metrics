228e38dd09ba517a32aa5e67efa5e898603f06b7
Ian <47256454+IanRFerguson@users.noreply.github.com>
2026-01-11 17:48:02 -0500

    hide in development

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -4,18 +4,28 @@ import WeeklyStats from './components/WeeklyStats.jsx';
 import PasswordGate from './components/PasswordGate.jsx';
 
 function App() {
+  const isDevelopment = import.meta.env.DEV;
+
+  const content = (
+    <>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <WeeklyStats />
+      </div>
+    </>
+  );
+
+  if (isDevelopment) {
+    return content;
+  }
+
   return (
     <PasswordGate password="shred">
-      <>
-        <div>
-          <Header />
-        </div>
-        <div>
-          <WeeklyStats />
-        </div>
-      </>
+      {content}
     </PasswordGate>
-  )
+  );
 }
 
 export default App
