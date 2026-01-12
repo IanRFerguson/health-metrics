5090a41298d4359d51fc3b1eb83ec687f7bf64ce
Ian <47256454+IanRFerguson@users.noreply.github.com>
2026-01-11 21:31:43 -0500

    add miles per day for plotting purposes

--- src/analytics/models/analytics/cln__metrics_per_day.sql
+++ src/analytics/models/analytics/cln__metrics_per_day.sql
@@ -16,10 +16,19 @@ WITH
             ROUND(SUM(sum_step_count), 3) AS total_step_count,
             MAX(weight_lb) AS max_weight_lb,
             ARRAY_CONCAT_AGG(daily_workouts) AS all_daily_workouts,
+            ROUND(
+                SUM(
+                    CASE 
+                        WHEN dw.workout_type LIKE '%RUN%' THEN dw.distance_in_miles 
+                        ELSE 0 
+                    END
+                )
+            , 3) AS total_miles_run,
             ARRAY_CONCAT_AGG(food_line_items) AS all_food_line_items,
             NULL AS food_score
         
-        FROM base
+        FROM base,
+            UNNEST(base.daily_workouts) AS dw
         GROUP BY target_date
     )
 
