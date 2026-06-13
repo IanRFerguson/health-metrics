d8d3b541c16628a25f57449380875b5d3fa7863f
Ian <47256454+IanRFerguson@users.noreply.github.com>
2026-06-12 22:28:11 -0400

    dbt ops

---
+++ src/analytics/macros/generate_schema_name.sql
@@ -0,0 +1,18 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    
+    {# If the model is in the "marts" directory, we want to omit the schema prefix #}
+    {%- set omit_schema_prefix = node.path.startswith("marts") and target.name == "cloud" -%}
+    
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ default_schema }}_{{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
+{%- endmacro %}
